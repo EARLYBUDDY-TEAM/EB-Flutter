ecf6006720a4b20782968592a60740b283695961
chminii <chminipark@gmail.com>
2024-03-31 14:04:17 +0900

    [Refactor] : 홈메뉴바 로그아웃 버튼 기능 임시 추가
    Git : #1

--- lib/presentation/home/view/home_view.dart
+++ lib/presentation/home/view/home_view.dart
@@ -9,41 +9,22 @@ part 'home_calendar.dart';
 part 'home_schedulecard.dart';
 part 'home_transportcard.dart';
 
-// class HomeView extends StatelessWidget {
-//   const HomeView({super.key});
+class MenuButton extends StatelessWidget {
+  final VoidCallback onPressed;
 
-//   static Route<void> route() {
-//     return MaterialPageRoute<void>(builder: (_) => const HomeView());
-//   }
+  const MenuButton({
+    super.key,
+    required this.onPressed,
+  });
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Padding(
-//         padding: const EdgeInsets.all(20),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Builder(
-//               builder: (context) {
-//                 final userID = context.select(
-//                   (AuthBloc bloc) => bloc.state.user.id,
-//                 );
-//                 return Text('UserID : $userID');
-//               },
-//             ),
-//             ElevatedButton(
-//               child: const Text('Logout'),
-//               onPressed: () {
-//                 context.read<AuthBloc>().add(AuthLogoutRequested());
-//               },
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: onPressed,
+      icon: const Icon(Icons.menu, color: Colors.white),
+    );
+  }
+}
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -60,11 +41,9 @@ class HomeView extends StatelessWidget {
         WaveBackground(),
         Scaffold(
           appBar: AppBar(
-            leading: const Icon(
-              Icons.menu,
-              color: Colors.white,
-              size: 30,
-            ),
+            leading: MenuButton(onPressed: () {
+              context.read<AuthBloc>().add(AuthLogoutRequested());
+            }),
             backgroundColor: Colors.transparent,
           ),
           backgroundColor: Colors.transparent,
