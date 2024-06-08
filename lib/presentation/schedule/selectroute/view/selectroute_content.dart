cefe6e1d5fb98a113f0ecddc3331a91888b12edb
chminii <chminipark@gmail.com>
2024-06-08 18:14:55 +0900

    [Design] : SelectRoute appbar 생성
    Git : #10

---
+++ lib/presentation/schedule/selectroute/view/selectroute_content.dart
@@ -0,0 +1,10 @@
+part of 'selectroute_view.dart';
+
+class _SelectRouteContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('_SelectRouteContent'),
+    );
+  }
+}
--- lib/presentation/schedule/selectroute/view/selectroute_view.dart
+++ lib/presentation/schedule/selectroute/view/selectroute_view.dart
@@ -1,14 +1,20 @@
+import 'package:earlybuddy/shared/eb_resources/eb_resources.dart';
 import 'package:flutter/material.dart';
+
+part 'selectroute_appbar.dart';
+part 'selectroute_content.dart';
 
 class SelectRouteView extends StatelessWidget {
   const SelectRouteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('SelectRoute'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: _SelectRouteAppBar(
+        cancelAction: () {},
       ),
+      body: _SelectRouteContent(),
     );
   }
 }
