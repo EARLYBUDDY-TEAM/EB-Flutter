bf6f3d1f9f7c6bf9050931d64725fbb91be13ba9
chminii <chminipark@gmail.com>
2024-08-30 20:33:20 +0900

    [Refactor] : 모듈화된 전체 프로젝트 교체
    Git : #17

--- android/app/src/main/kotlin/com/example/earlybuddy/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.earlybuddy
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/core/network/network.dart
+++
@@ -1,2 +0,0 @@
-export 'service/service.dart';
-export 'endpoint/endpoint.dart';
