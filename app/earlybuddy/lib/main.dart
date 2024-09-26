55997d9c54810be65cf0ef2134d2462261678eb0
chminii <chminipark@gmail.com>
2024-09-26 19:53:37 +0900

    [Feat] : isFirstLaunch 로직 추가
    Git : #21

--- app/earlybuddy/lib/main.dart
+++ app/earlybuddy/lib/main.dart
@@ -1,25 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:eb_root/eb_root.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:eb_secure_storage/eb_secure_storage.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   widgetsBinding = await PrepareRoot.setup(widgetsBinding: widgetsBinding);
-  FlutterNativeSplash.remove();
   final isFirstLaunch = await checkFirstLaunch();
+  FlutterNativeSplash.remove();
   isFirstLaunch ? runApp(const OnboardingView()) : runApp(RootView());
-}
-
-Future<bool> checkFirstLaunch() async {
-  final secureStorage = SecureStorage();
-  try {
-    await secureStorage.read(key: SecureStorageKey.isFirstLaunch);
-    return false;
-  } catch (e) {
-    await secureStorage.write(
-        key: SecureStorageKey.isFirstLaunch, value: "true");
-    return true;
-  }
 }
--- presentation/root/views/lib/eb_root.dart
+++ presentation/root/views/lib/eb_root.dart
@@ -9,6 +9,7 @@ import 'package:eb_env/eb_env.dart';
 import 'package:eb_location/eb_location.dart';
 import 'package:eb_search_place/eb_search_place.dart';
 import 'package:eb_state/eb_state.dart';
+import 'package:eb_secure_storage/eb_secure_storage.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -16,12 +17,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'sources/root/root_view.dart';
-part 'sources/splash/splash_view.dart';
 part 'sources/root/root_bloc_view.dart';
 part 'sources/onboarding/onboarding_view.dart';
-part 'sources/compose_view.dart';
 
 final class PrepareRoot {
   static Future<WidgetsBinding> setup({
@@ -39,5 +39,27 @@ final class PrepareRoot {
     await LocationProvider.shared.checkPermission();
 
     return myWidgetsBinding;
+  }
+}
+
+Future<bool> checkFirstLaunch() async {
+  final List<SecureStorageKey> keysToEliminate = [
+    SecureStorageKey.accessToken,
+    SecureStorageKey.refreshToken,
+  ];
+
+  final prefs = await SharedPreferences.getInstance();
+
+  if (prefs.getBool('first_run') ?? true) {
+    final secureStorage = SecureStorage();
+    await Future.wait(
+      keysToEliminate.map(
+        (key) => secureStorage.delete(key: key),
+      ),
+    );
+    prefs.setBool('first_run', false);
+    return true;
+  } else {
+    return false;
   }
 }
