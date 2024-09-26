4baea72874da8221b455444e1b210894631b568b
chminii <chminipark@gmail.com>
2024-09-26 16:43:13 +0900

    [Feat] : SplashView show only first launch
    Git : #21

--- app/earlybuddy/lib/main.dart
+++ app/earlybuddy/lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:eb_root/eb_root.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:eb_secure_storage/eb_secure_storage.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   widgetsBinding = await PrepareRoot.setup(widgetsBinding: widgetsBinding);
   FlutterNativeSplash.remove();
-  runApp(const OnboardingView());
+  final isFirstLaunch = await checkFirstLaunch();
+  isFirstLaunch ? runApp(const OnboardingView()) : runApp(RootView());
+}
+
+Future<bool> checkFirstLaunch() async {
+  final secureStorage = SecureStorage();
+  try {
+    await secureStorage.read(key: SecureStorageKey.isFirstLaunch);
+    return false;
+  } catch (e) {
+    await secureStorage.write(
+        key: SecureStorageKey.isFirstLaunch, value: "true");
+    return true;
+  }
 }
