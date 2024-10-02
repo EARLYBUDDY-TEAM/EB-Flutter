69eb44f175d8849129bfca0a7e21395b00fff99e
chminii <chminipark@gmail.com>
2024-10-02 16:45:13 +0900

    [Refactor] : withCheckToken 로직 수정
    Git : #27

--- domain/event/features/lib/eb_event.dart
+++ domain/event/features/lib/eb_event.dart
@@ -2,5 +2,7 @@ import 'package:eb_uikit/eb_uikit.dart';
 import 'package:eb_delegate/eb_delegate.dart';
 import 'package:eb_repository/eb_repository.dart';
 import 'package:eb_state/eb_state.dart';
+import 'package:eb_secure_storage/eb_secure_storage.dart';
+import 'package:eb_model/eb_model.dart';
 
 part 'sources/token_event.dart';
