9d003baf10d4c17f63cbcc239eee6967555cf443
chminii <chminipark@gmail.com>
2024-10-03 22:01:13 +0900

    [Fix] : fix feature BottomScheduleCardListView swipe delete
    Git : #27

--- presentation/home/examples/lib/sources/mock_bottom_schedulecardview.dart
+++ presentation/home/examples/lib/sources/mock_bottom_schedulecardview.dart
@@ -16,13 +16,19 @@ final class MockBottomScheduleCardExample extends StatelessWidget {
   }
 }
 
-final class _MockBottomScheduleCardListView extends StatelessWidget {
+final class _MockBottomScheduleCardListView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MockBottomScheduleCardListState();
+}
+
+final class _MockBottomScheduleCardListState
+    extends State<_MockBottomScheduleCardListView> {
   final items = List<ScheduleCard>.generate(20, (i) {
     bool flag = (i + 1) % 2 == 0 ? true : false;
     return flag ? ScheduleCard.mock() : ScheduleCard.mockwithPlace();
   });
 
-  _MockBottomScheduleCardListView();
+  _MockBottomScheduleCardListState();
 
   @override
   Widget build(BuildContext context) {
@@ -32,13 +38,31 @@ final class _MockBottomScheduleCardListView extends StatelessWidget {
         return const SizedBox(height: 20);
       },
       itemBuilder: (contex, index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: BottomScheduleCardView(
-            scheduleCard: items[index],
-          ),
+        final item = items[index];
+        return Dismissible(
+          // key: Key(item.scheduleID.toString()),
+          key: UniqueKey(),
+          onDismissed: (direction) {
+            setState(() {
+              items.removeAt(index);
+            });
+
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('MockCard $index dismissed')));
+          },
+          background: Container(color: Colors.red),
+          child: _bottomScheduleCardView(item),
         );
       },
+    );
+  }
+
+  Widget _bottomScheduleCardView(ScheduleCard scheduleCard) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: BottomScheduleCardView(
+        scheduleCard: scheduleCard,
+      ),
     );
   }
 }
