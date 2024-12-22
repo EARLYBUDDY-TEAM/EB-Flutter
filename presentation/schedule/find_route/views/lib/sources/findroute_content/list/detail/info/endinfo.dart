8fd4638055e0f8fec4e0b923e7fea9045442a937
chminii <chminipark@gmail.com>
2024-12-22 17:05:14 +0900

    [Fix] : Add flexible to endinfo in detailroute
    Git : #51

--- presentation/schedule/find_route/views/lib/sources/findroute_content/list/detail/info/endinfo.dart
+++ presentation/schedule/find_route/views/lib/sources/findroute_content/list/detail/info/endinfo.dart
@@ -88,14 +88,16 @@ final class _EndInfoOther extends StatelessWidget {
           fontSize: fontSize - 2,
         ),
         const SizedBox(width: 8),
-        Text(
-          endName,
-          style: TextStyle(
-            fontFamily: FontFamily.nanumSquareBold,
-            fontSize: fontSize,
-            color: EBColors.text,
+        Flexible(
+          child: Text(
+            endName,
+            style: TextStyle(
+              fontFamily: FontFamily.nanumSquareBold,
+              fontSize: fontSize,
+              color: EBColors.text,
+            ),
+            overflow: TextOverflow.ellipsis,
           ),
-          overflow: TextOverflow.ellipsis,
         ),
       ],
     );
